2a6a3acb995a0cc5f66ee2a60c4960f1525f41ab
Michael Gao <mgao.ucla@gmail.com>
2013-01-06 20:40:06 +0800

    first commit. just landed in beijing from ski resort in DongBei

---
+++ Piggyback/Piggyback/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Piggyback
+//
+//  Created by Michael Gao on 1/6/13.
+//  Copyright (c) 2013 Piggyback. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
